d3d5a6e3a52233ed7a8ba1039d00e84c9921dea6
Nguyễn Tiến Trung Kiên <99466207+kientee@users.noreply.github.com>
2023-05-20 22:17:54 +0700

    update vinpearl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
 
-  BASE_PATH_DOMAIN: "https://192.168.1.3:8443/api/v1",
+  // BASE_PATH_DOMAIN: "https://192.168.1.3:8443/api/v1",
+  BASE_PATH_DOMAIN: "http://localhost:8080/api/v1",
+  // BASE_PATH_DOMAIN: "https://localhost:8443/api/v1",
 };
